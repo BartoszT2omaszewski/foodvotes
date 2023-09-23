584eabd7d724b70da8ab8b4387c9c224569dbc9a
Paweł Dudzik <Gamer710@wp.pl>
2023-09-23 13:05:15 +0200

    moved sort widget to new file

--- lib/app/home/opinie/votes_page_content.dart
+++ lib/app/home/opinie/votes_page_content.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:najlepsza_pizza_w_miescie/app/home/opinie/cubit/votes_cubit.dart';
 import 'package:najlepsza_pizza_w_miescie/app/home/opinie/cubit/votes_state.dart';
+import 'package:najlepsza_pizza_w_miescie/app/home/opinie/sort_page.dart';
 
 class VotesPageContent extends StatefulWidget {
   const VotesPageContent({
@@ -18,7 +19,7 @@ class _VotesPageContentState extends State<VotesPageContent> {
     return BlocProvider<VotesCubit>(
       create: (context) => VotesCubit()..start(),
       child: Container(
-        padding: const EdgeInsets.all(20.0), // Padding dla całej zawartości
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
             const Padding(
@@ -29,34 +30,7 @@ class _VotesPageContentState extends State<VotesPageContent> {
                 textAlign: TextAlign.center,
               ),
             ),
-            Builder(
-              builder: (newContext) {
-                return DropdownButton<String>(
-                  value: 'rating',
-                  items: const [
-                    DropdownMenuItem(
-                      value: 'rating',
-                      child: Text(
-                        'Sortuj według (Od najniższej)',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                    DropdownMenuItem(
-                      value: 'rating_desc',
-                      child: Text(
-                        'Sortuj według (Od najwyższej)',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ],
-                  onChanged: (String? newValue) {
-                    if (newValue != null) {
-                      newContext.read<VotesCubit>().changeSorting(newValue);
-                    }
-                  },
-                );
-              },
-            ),
+            const SortWidget(),
             const SizedBox(
               height: 18,
             ),
@@ -92,54 +66,128 @@ class _VotesPageContentState extends State<VotesPageContent> {
                     for (final document in documents) ...[
                       Padding(
                         padding: const EdgeInsets.all(20.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.white,
-                            ),
-                            borderRadius: BorderRadius.circular(15.0),
-                          ),
-                          padding: const EdgeInsets.all(12.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  'Nazwa restauracji: ${document['name']}',
-                                  style: const TextStyle(color: Colors.white),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.white,
                                 ),
+                                borderRadius: BorderRadius.circular(15.0),
                               ),
-                              const SizedBox(
-                                height: 25,
+                              padding: const EdgeInsets.all(12.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      'Nazwa restauracji: ${document['name']}',
+                                      style:
+                                          const TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 25,
+                                  ),
+                                  Text(
+                                    'Miasto: ${document['location']}',
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                  const SizedBox(
+                                    height: 15,
+                                  ),
+                                  Text(
+                                    'Opis: ${document['description']}',
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                  const SizedBox(
+                                    height: 15,
+                                  ),
+                                  Text(
+                                    'Danie: ${document['pizza']}',
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                  const SizedBox(
+                                    height: 15,
+                                  ),
+                                  Text(
+                                    'Ocena: ${document['rating'].toString()}',
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                ],
                               ),
-                              Text(
-                                'Miasto: ${document['location']}',
-                                style: const TextStyle(color: Colors.white),
-                              ),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              Text(
-                                'Opis: ${document['description']}',
-                                style: const TextStyle(color: Colors.white),
-                              ),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              Text(
-                                'Danie: ${document['pizza']}',
-                                style: const TextStyle(color: Colors.white),
-                              ),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              Text(
-                                'Ocena: ${document['rating'].toString()}',
-                                style: const TextStyle(color: Colors.white),
-                              ),
-                            ],
-                          ),
+                            ),
+                            const SizedBox(height: 8.0),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    //logika
+                                  },
+                                  child: const Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      Text(
+                                        'Głos',
+                                        style: TextStyle(
+                                            color: Color.fromARGB(
+                                                255, 250, 249, 249)),
+                                      ),
+                                      Icon(
+                                        Icons.arrow_drop_up,
+                                        color: Color.fromARGB(255, 0, 0, 0),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(width: 16.0),
+                                GestureDetector(
+                                  onTap: () {
+                                    //logika
+                                  },
+                                  child: const Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      Text(
+                                        'Głos',
+                                        style: TextStyle(
+                                            color: Color.fromARGB(
+                                                255, 250, 249, 249)),
+                                      ),
+                                      Icon(
+                                        Icons.arrow_drop_down,
+                                        color: Color.fromARGB(255, 0, 0, 0),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(width: 16.0),
+                                GestureDetector(
+                                  onTap: () {
+                                    //logika
+                                  },
+                                  child: const Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      Text(
+                                        'Komentarz',
+                                        style: TextStyle(
+                                            color: Color.fromARGB(
+                                                255, 250, 249, 249)),
+                                      ),
+                                      Icon(
+                                        Icons.arrow_drop_down,
+                                        color: Color.fromARGB(255, 0, 0, 0),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
                       ),
                     ],
