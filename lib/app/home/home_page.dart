75d25566327f140aaaff863157c5f5f7655999b2
Dawid Szosziaszwili <dawidszwili7@gmail.com>
2023-10-01 06:53:45 +0200

    Add person icon to appbar

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -26,6 +26,15 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.person_2,
+              color: Colors.white,
+            ),
+          ),
+        ],
         title: Row(
           children: [
             RichText(
